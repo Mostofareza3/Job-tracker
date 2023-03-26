857601f59d8007767aaf2a30170db91abf91f347
mostofa-reza <86896410+Mostofareza3@users.noreply.github.com>
2023-03-26 16:32:31 +0600

    ready for production

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,40 @@
+import main from "../assets/images/main.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
+import { Logo } from "../components";
+import { Link } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+import { useAppContext } from "../context/appContext";
+import React from "react";
+
+const Landing = () => {
+  const { user } = useAppContext();
+  return (
+    <React.Fragment>
+      {user && <Navigate to="/" />}
+      <Wrapper>
+        <nav>
+          <Logo />
+        </nav>
+        <div className="container page">
+          {/* info */}
+          <div className="info">
+            <h1>
+              job <span>tracking</span> app
+            </h1>
+            <p>
+              Organize and track your job search with ease using our app. Stay
+              on top of application deadlines, interviews, and offers all in one
+              place. Take control of your job search today.
+            </p>
+            <Link to="/register" className="btn btn-hero">
+              Login/Register
+            </Link>
+          </div>
+          <img src={main} alt="job hunt" className="img main-img" />
+        </div>
+      </Wrapper>
+    </React.Fragment>
+  );
+};
+
+export default Landing;
